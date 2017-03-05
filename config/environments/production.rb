029fd659ec54ee2e13dd28212bbe3ae1dd37a0a3
Konstantin M <sub@linuxpath.com>
2017-03-05 11:01:58 -0800

    Fixed production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,13 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.fastmail.com',
+  port:                 587,
+  domain:               'myflix.local',
+  user_name:            ENV["fm_user"],
+  password:             ENV["fm_password"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
