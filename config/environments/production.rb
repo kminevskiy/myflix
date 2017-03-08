7ab693b93c8786c5506e68a38db6e1f8b5739cb8
Konstantin M <sub@linuxpath.com>
2017-03-08 11:25:52 -0800

    mailgun heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,11 +19,11 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.fastmail.com',
-  port:                 587,
-  domain:               'myflix.local',
-  user_name:            ENV["fm_user"],
-  password:             ENV["fm_password"],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'fast-coast-16993.herokuapp.com', #mydomain actually contains the realvalue
+    :authentication => :plain,
+  }
 end
